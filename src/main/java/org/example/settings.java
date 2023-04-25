1dec055bb8754dd61924cb2082d863d4197d1b24
maggiezetaa <16521475@std.stei.itb.ac.id>
2023-04-25 20:38:55 +0700

    feat: settings-ui | (-) .java code

---
+++ src/main/java/org/example/settings.java
@@ -0,0 +1,25 @@
+package org.example;
+import javax.swing.*;
+
+public class settings {
+    private JLabel namaApp;
+    private JButton kasirButton;
+    private JButton transaksiButton;
+    private JButton laporanButton;
+    private JButton memberButton;
+    private JButton inventarisButton;
+    private JButton pengaturanButton;
+    private JButton homeButton;
+    private JButton a3dotsButton;
+    private JButton GUI1Button;
+    private JButton pengaturanButton1;
+    private JTextField textField1;
+    private JRadioButton JSONRadioButton;
+    private JRadioButton XMLRadioButton;
+    private JTextField textField2;
+    private JButton tambahkanBaruButton;
+    private JLabel pengaturanLabel;
+    private JLabel formatFile;
+    private JLabel pluginLabel;
+    private JLabel tempatPenyimpananFileLabel;
+}
