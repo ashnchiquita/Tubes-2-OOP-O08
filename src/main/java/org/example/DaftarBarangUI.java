0c869fa46cd033a66e8e665841a8b34cd6d7e4e1
Alisha Listya <alisha.listya@gmail.com>
2023-04-25 12:39:13 +0700

    fix: refactoring DaftarBarangUI into Panel

--- src/main/java/org/example/DaftarBarangUI.java
+++ src/main/java/org/example/DaftarBarangUI.java
@@ -3,111 +3,94 @@ package org.example;
 import javax.swing.*;
 import javax.swing.border.Border;
 import javax.swing.plaf.basic.BasicButtonUI;
+import javax.swing.table.DefaultTableCellRenderer;
 import javax.swing.table.JTableHeader;
+import javax.swing.table.TableCellRenderer;
 import java.awt.*;
 import java.awt.geom.RoundRectangle2D;
-import java.io.File;
 
-public class DaftarBarangUI extends JFrame{
-    private static JFrame mainFrame;
-    private static JPanel mainRight;
+public class DaftarBarangUI extends JPanel{
+    private static JPanel headerPanel;
+    private static JScrollPane scrollListPanel;
 
-    private static void setupMainFrame() {
-        // Setup Main Frame
-        mainFrame = new JFrame("Test Window");
-        mainFrame.setSize(1280, 720);
-        mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        mainFrame.setLayout(new BorderLayout());
-        mainFrame.getContentPane().setBackground(Color.LIGHT_GRAY);
-    }
-    private static void setupRightPanel(){
-
-        // Setup mainRight Panel
-        mainRight = new JPanel();
-        mainRight.setLayout(new BorderLayout());
-        mainRight.setBorder(BorderFactory.createEmptyBorder(10, 20, 10, 20));
-        mainFrame.add(mainRight, BorderLayout.CENTER);
-
-    }
-
-    private static void showUI() {
-        setupMainFrame();
-        setupRightPanel();
-
-        // Header Panel
-        JPanel headerPanel = new JPanel();
-        Border paddingBorder = BorderFactory.createEmptyBorder(75, 50, 60, 50);
+    private void setupHeaderPanel(){
+        headerPanel = new JPanel();
+        Border paddingBorder = BorderFactory.createEmptyBorder(75, 20, 60, 20);
         headerPanel.setBackground(new Color(255,255,255));
         headerPanel.setPreferredSize(new Dimension(1045,168));
         headerPanel.setBorder(paddingBorder);
-        mainRight.add(headerPanel,BorderLayout.NORTH);
 
+        // Label "Daftar Barang"
+        JLabel daftarBarangLabel = new JLabel("Daftar Barang");
+        daftarBarangLabel.setHorizontalAlignment(SwingConstants.LEFT);
+        daftarBarangLabel.setForeground(new Color(229, 151, 0));
+        daftarBarangLabel.setFont(new Font("Inter", Font.BOLD, 33));
+        headerPanel.add(daftarBarangLabel, BorderLayout.WEST);
 
-            // Label "Daftar Barang"
-            JLabel daftarBarangLabel = new JLabel("Daftar Barang");
-            daftarBarangLabel.setHorizontalAlignment(SwingConstants.LEFT);
-            daftarBarangLabel.setForeground(new Color(229, 151, 0));
-            daftarBarangLabel.setFont(new Font("Inter", Font.BOLD, 33));
-            headerPanel.add(daftarBarangLabel, BorderLayout.WEST);
+        // Horizontal Divider
+        JSeparator separator1 = new JSeparator(SwingConstants.HORIZONTAL);
+        separator1.setPreferredSize(new Dimension(150, 1));
+        separator1.setVisible(true);
+        headerPanel.add(separator1);
 
-            // Horizontal Divider
-            JSeparator separator1 = new JSeparator(SwingConstants.HORIZONTAL);
-            separator1.setPreferredSize(new Dimension(168, 1));
-            separator1.setVisible(true);
-            headerPanel.add(separator1);
+        // Label "Total Barang"
+        JLabel totalBarangLabel = new JLabel("Total Barang : 127");
+        totalBarangLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        totalBarangLabel.setPreferredSize(new Dimension(180,42));
+        totalBarangLabel.setFont(new Font("Inter", Font.PLAIN, 15));
+        totalBarangLabel.setForeground(new Color(36,60,148));
+        totalBarangLabel.setBorder(BorderFactory.createLineBorder(new Color(0x5D82E8)));
+        headerPanel.add(totalBarangLabel, BorderLayout.WEST);
 
-            // Label "Total Barang"
-            JLabel totalBarangLabel = new JLabel("Total Barang : 127");
-            totalBarangLabel.setHorizontalAlignment(SwingConstants.LEFT);
-            totalBarangLabel.setFont(new Font("Inter", Font.PLAIN, 18));
-            totalBarangLabel.setForeground(new Color(36,60,148));
-            totalBarangLabel.setBorder(BorderFactory.createLineBorder(Color.BLACK));
-            headerPanel.add(totalBarangLabel, BorderLayout.WEST);
+        // Horizontal Divider
+        JSeparator separator2 = new JSeparator(SwingConstants.HORIZONTAL);
+        separator2.setPreferredSize(new Dimension(20, 1));
+        separator2.setVisible(true);
+        headerPanel.add(separator2);
 
-            // Horizontal Divider
-            JSeparator separator2 = new JSeparator(SwingConstants.HORIZONTAL);
-            separator2.setPreferredSize(new Dimension(25, 1));
-            separator2.setVisible(true);
-            headerPanel.add(separator2);
+        // Button Import
+        JButton importButton = new JButton("Import");
+        CustomButtonUI buttonUI = new CustomButtonUI(new Color(45,77,182));
+        importButton.setFont(new Font("Inter", Font.PLAIN, 15));
+        importButton.setBackground(new Color(76,110,223));
+        importButton.setForeground(Color.WHITE);
+        importButton.setOpaque(true);
+        importButton.setFocusable(false);
+        importButton.setPreferredSize(new Dimension(130,43));
+        importButton.setUI(buttonUI);
+        importButton.setBorder(new RoundBorder(20));
+        headerPanel.add(importButton, BorderLayout.WEST);
 
-            // Button Import
-            JButton importButton = new JButton("Import");
-            CustomButtonUI buttonUI = new CustomButtonUI(new Color(45,77,182));
-            importButton.setFont(new Font("Inter", Font.PLAIN, 18));
-            importButton.setBackground(new Color(76,110,223));
-            importButton.setForeground(Color.WHITE);
-            importButton.setOpaque(true);
-            importButton.setFocusable(false);
-            importButton.setPreferredSize(new Dimension(174,43));
-            importButton.setUI(buttonUI);
-            importButton.setBorder(new RoundBorder(20));
-            headerPanel.add(importButton, BorderLayout.WEST);
+        // Horizontal Divider
+        JSeparator separator3 = new JSeparator(SwingConstants.HORIZONTAL);
+        separator3.setPreferredSize(new Dimension(20, 1));
+        separator3.setVisible(true);
+        headerPanel.add(separator3);
 
-            // Horizontal Divider
-            JSeparator separator3 = new JSeparator(SwingConstants.HORIZONTAL);
-            separator3.setPreferredSize(new Dimension(25, 1));
-            separator3.setVisible(true);
-            headerPanel.add(separator3);
+        // Button Tambah Baru
+        JButton createNewItemButton = new JButton("+ Tambah Baru");
+        createNewItemButton.setFont(new Font("Inter", Font.PLAIN, 15));
+        createNewItemButton.setBackground(new Color(76,110,223));
+        createNewItemButton.setForeground(Color.WHITE);
+        createNewItemButton.setOpaque(true);
+        createNewItemButton.setFocusable(false);
+        createNewItemButton.setPreferredSize(new Dimension(180,43));
+        createNewItemButton.setUI(buttonUI);
+        createNewItemButton.setBorder(new RoundBorder(20));
+        headerPanel.add(createNewItemButton, BorderLayout.WEST);
+    }
 
-            // Button Tambah Baru
-            JButton createNewItemButton = new JButton("+ Tambah Baru");
-            createNewItemButton.setFont(new Font("Inter", Font.PLAIN, 18));
-            createNewItemButton.setBackground(new Color(76,110,223));
-            createNewItemButton.setForeground(Color.WHITE);
-            createNewItemButton.setOpaque(true);
-            createNewItemButton.setFocusable(false);
-            createNewItemButton.setPreferredSize(new Dimension(174,43));
-            createNewItemButton.setUI(buttonUI);
-            createNewItemButton.setBorder(new RoundBorder(20));
-            headerPanel.add(createNewItemButton, BorderLayout.WEST);
-
+    private void setupTable(){
         // Table
-        JScrollPane scrollListPanel = new JScrollPane();
+        scrollListPanel = new JScrollPane();
         scrollListPanel.setBackground(Color.LIGHT_GRAY);
-        mainRight.add(scrollListPanel, BorderLayout.CENTER);
+        scrollListPanel.setPreferredSize(new Dimension(1045,515));
+
+        // Cell Renderer
+
 
         JTable itemList = new JTable(getData(), getColumnNames());
-        itemList.setRowHeight(36);
+        itemList.setRowHeight(136);
         itemList.setDefaultEditor(Object.class, null); // make cells not editable
         itemList.getColumnModel().getColumn(0).setPreferredWidth(300);
         itemList.getColumnModel().getColumn(1).setPreferredWidth(300);
@@ -121,17 +104,27 @@ public class DaftarBarangUI extends JFrame{
         itemList.setGridColor(new Color(240, 240, 240));
         itemList.setIntercellSpacing(new Dimension(0, 5));
 
+        // create a custom header renderer that doesn't paint any effects
+        TableCellRenderer headerRenderer = new DefaultTableCellRenderer() {
+            @Override
+            public Component getTableCellRendererComponent(JTable table, Object value,
+                                                           boolean isSelected, boolean hasFocus, int row, int column) {
+                super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
+                setBorder(BorderFactory.createEmptyBorder());
+                setHorizontalAlignment(JLabel.CENTER);
+                setBackground(new Color(0xDAE2FF));
+                setForeground(new Color(0x243C94));
+                setFont(new Font("Inter", Font.BOLD, 12));
+                return this;
+            }
+        };
+
         // Set blue background color for table header
         JTableHeader tableHeader = itemList.getTableHeader();
-        tableHeader.setBackground(new Color(0xDAE2FF));
-        tableHeader.setForeground(new Color(0x243C94)); // set the color of the header text
+        tableHeader.setDefaultRenderer(headerRenderer);
+        tableHeader.setAlignmentX(10);
         tableHeader.setFont(tableHeader.getFont().deriveFont(Font.BOLD, 14));
         tableHeader.setPreferredSize(new Dimension(994, 43));
-        tableHeader.setBorder(BorderFactory.createLineBorder(Color.BLACK));
-//        tableHeader.setBorder(BorderFactory.createEmptyBorder(5, 50, 5, 5));
-//       tableHeader.setBorder(new Color(240, 240, 240));
-
-        mainFrame.setVisible(true);
     }
 
     private static Object[][] getData() {
@@ -156,20 +149,17 @@ public class DaftarBarangUI extends JFrame{
         return columnNames;
     }
 
-
-
-    public static void main(String[] args) {
-
-        javax.swing.SwingUtilities.invokeLater(new Runnable() {
-            public void run() {
-                showUI();
-            }
-        });
+    public DaftarBarangUI(){
+        this.setBackground(Color.WHITE);
+        setupHeaderPanel();
+        this.add(headerPanel,BorderLayout.NORTH);
+        setupTable();
+        this.add(scrollListPanel, BorderLayout.CENTER);
     }
 }
 
 class RoundBorder implements Border {
-    private int radius;
+    private final int radius;
 
     public RoundBorder(int radius) {
         this.radius = radius;
@@ -195,7 +185,7 @@ class RoundBorder implements Border {
 
 class CustomButtonUI extends BasicButtonUI {
 
-    private Color pressedBackgroundColor;
+    private final Color pressedBackgroundColor;
 
     public CustomButtonUI(Color pressedBackgroundColor) {
         this.pressedBackgroundColor = pressedBackgroundColor;
