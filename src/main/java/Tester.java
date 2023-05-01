46275f3ba78eab7f2cf7ccc9d72006c1e6dd3126
Rava Maulana <ravamaulana14@gmail.com>
2023-05-01 19:57:08 +0700

    feat: added basic controller methods

--- src/main/java/Tester.java
+++ src/main/java/Tester.java
@@ -5,10 +5,6 @@ public class Tester {
   public static void main(String[] args) {
     MemberController controller = new MemberController();
     controller.setDataIO(new MemberAdapterXML("src/main/resources/data/member.xml"));
-    // controller.showByID(14);
-    // controller.showAllMember();
-    controller.addMember();
-    // controller.updateMember();
-    // controller.deleteMember(11);
+
   }
 }
--- src/main/java/controller/member/MemberController.java
+++ src/main/java/controller/member/MemberController.java
@@ -1,7 +1,6 @@
 package controller.member;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 import lombok.Setter;
 import model.Member;
@@ -10,29 +9,48 @@ public class MemberController {
   @Setter
   MemberDataIO dataIO;
 
-  public void showByID(int id) {
-    System.out.println(dataIO.getByID(id));
-  }
-
-  public void showAllMember() {
+  public List<Member> getAllMember() {
     List<Member> memberList = new ArrayList<>();
     memberList = dataIO.getAllMembers();
-    memberList.stream().forEach(member -> System.out.println(member));
+    return memberList;
   }
 
-  public void addMember() {
-    Member newMember = Member.builder().id(11).name("Kim Jisoo").phone("123").point(100).transactions(10).build();
-    System.out.println(dataIO.insertMember(newMember));
+  public boolean addNewMember(String name, String phone) {
+    List<Member> memberList = dataIO.getAllMembers();
+    int id = memberList.size() + 1;
+
+    Member newMember = Member.builder().id(id).name(name).phone(phone).point(0).transactions(1).build();
+    return dataIO.insertMember(newMember);
   }
 
-  public void updateMember(int id) {
-    Member newDataMember = Member.builder().id(id).name("Kim Jisoo Unnieeeeeeeeeeeee").phone("123").point(100)
-        .transactions(10)
-        .build();
-    System.out.println(dataIO.updateMember(11, newDataMember));
+  public boolean addTransactions(int id) {
+    Member member = dataIO.getByID(id);
+    if (member == null) {
+      return false;
+    }
+
+    member.setTransactions(member.getTransactions() + 1);
+    return dataIO.updateMember(id, member);
   }
 
-  public void deleteMember(int id) {
-    System.out.println(dataIO.deleteMember(id));
+  public boolean addPoint(int id, int point) {
+    Member member = dataIO.getByID(id);
+    if (member == null) {
+      return false;
+    }
+
+    member.setPoint(member.getPoint() + 1);
+    return dataIO.updateMember(id, member);
+  }
+
+  public boolean updateMemberData(int id, String name, String phone) {
+    Member member = dataIO.getByID(id);
+    if (member == null) {
+      return false;
+    }
+
+    member.setName(name);
+    member.setPhone(phone);
+    return dataIO.updateMember(id, member);
   }
 }
