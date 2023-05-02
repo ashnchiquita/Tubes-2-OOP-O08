9c4eb496d6f0321d8ff7455be1c918fbc2295b7f
Alisha Listya <73476678+alishalistyaa@users.noreply.github.com>
2023-05-02 18:03:54 +0700

    Delete src/main/java/Boundary directory

--- src/main/java/Boundary/enums/PanelEnum.java
+++
@@ -1,8 +0,0 @@
-package boundary.enums;
-
-// Press ⇧ twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
-public enum PanelEnum {
-    HOME,
-    TEST
-}
--- src/main/java/Boundary/observer/pembelian/PembelianListener.java
+++
@@ -1,7 +0,0 @@
-package boundary.observer.pembelian;
-
-public interface PembelianListener {
-  void handleAddItem(PembelianEvent e);
-
-  void handleRemoveItem(PembelianEvent e);
-}
