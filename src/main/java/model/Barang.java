64404e8c5753feea86ffa77d7c659fce497843ac
vanessrw <64979714+vanessrw@users.noreply.github.com>
2023-05-03 09:31:06 +0700

    fix: barang model

--- src/main/java/model/Barang.java
+++ src/main/java/model/Barang.java
@@ -1,14 +1,41 @@
 package model;
 
-import java.io.Serializable;
-
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.time.LocalDate;
+import java.time.LocalTime;
 
 @Data
 @SuperBuilder(toBuilder = true)
-public class Barang implements Serializable{
-    private final int id;
-    private String name, kategori, gambar;
-    private int hargaJual, hargaBeli, jumlah;
+@NoArgsConstructor
+public class Barang implements Serializable {
+
+    @JsonIgnore
+    private static final long serialVersionUID = 3L;
+    @JsonProperty("id")
+    private int id;
+    @JsonProperty("name")
+    private String name;
+    @JsonProperty("kategori")
+    private String kategori;
+    @JsonProperty("hargaJual")
+    private Integer hargaJual;
+    @JsonProperty("hargaBeli")
+    private Integer hargaBeli;
+    @JsonProperty("jumlah")
+    private Integer jumlah;
+    @JsonProperty("gambar")
+    private String gambar;
+
+    public void addJumlah(int jumlah) {
+        this.jumlah += jumlah;
+    }
+
 }
