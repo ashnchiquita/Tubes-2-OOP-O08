11724ed02bf65ba021e2dcb7c256b6068a0b01b7
vanessrw <64979714+vanessrw@users.noreply.github.com>
2023-05-04 19:28:42 +0700

    fix: button in table

--- src/main/java/Boundary/panel/member/DaftarMemberPanel.java
+++ src/main/java/Boundary/panel/member/DaftarMemberPanel.java
@@ -1,30 +1,34 @@
-package boundary.panel.member;
+package boundary.panel.member.subpanel;
 
-import boundary.widget.PressedButton;
-import boundary.widget.RoundBorder;
-import boundary.widget.RoundedPanel;
+import boundary.constants.Colors;
+import boundary.constants.ResourcePath;
+import boundary.widget.*;
 
 import javax.swing.*;
 import javax.swing.border.Border;
-import javax.swing.table.DefaultTableCellRenderer;
 import javax.swing.table.JTableHeader;
 import javax.swing.table.TableCellRenderer;
 import javax.swing.table.TableColumn;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
-public class DaftarMemberPanel extends JPanel{
+
+public class DaftarMemberPanel extends FlowablePane {
     private static JPanel headerPanel;
     private static JScrollPane scrollListPanel;
     private RoundedPanel createNewItemButtonPanel = new RoundedPanel(25, new Color(0x4C6EDF), false, Color.WHITE,  0);
     private RoundedPanel importButtonPanel = new RoundedPanel(25, new Color(0x4C6EDF), false, Color.WHITE,  0);
     private RoundedPanel totalBarangPanel = new RoundedPanel(25, Color.WHITE, true, new Color(0x5D82E8),  2);
     private RoundedPanel periksaPanel = new RoundedPanel(25, new Color(0x4C6EDF), false, Color.WHITE,  0);
-    
+
     private void setupHeaderPanel(){
         headerPanel = new JPanel();
-        Border paddingBorder = BorderFactory.createEmptyBorder(75, 20, 60, 20);
+        Border paddingBorder = BorderFactory.createEmptyBorder(75, 40, 60, 20);
         headerPanel.setBackground(new Color(255,255,255));
-        headerPanel.setPreferredSize(new Dimension(1045,168));
+        headerPanel.setPreferredSize(new Dimension(1000,168));
         headerPanel.setBorder(paddingBorder);
 
         // Label "Daftar Barang"
@@ -36,8 +40,7 @@ public class DaftarMemberPanel extends JPanel{
 
         // Horizontal Divider
         JSeparator separator1 = new JSeparator(SwingConstants.HORIZONTAL);
-        separator1.setPreferredSize(new Dimension(150, 1));
-        separator1.setVisible(true);
+        separator1.setPreferredSize(new Dimension(280, 0));
         headerPanel.add(separator1);
 
         // Label "Total Barang"
@@ -51,7 +54,7 @@ public class DaftarMemberPanel extends JPanel{
 
         // Horizontal Divider
         JSeparator separator2 = new JSeparator(SwingConstants.HORIZONTAL);
-        separator2.setPreferredSize(new Dimension(20, 1));
+        separator2.setPreferredSize(new Dimension(20, 0));
         separator2.setVisible(true);
         headerPanel.add(separator2);
 
@@ -66,12 +69,13 @@ public class DaftarMemberPanel extends JPanel{
         importButton.setPreferredSize(new Dimension(130,38));
         importButton.setUI(buttonUI);
         importButton.setBorder(new RoundBorder(20));
+        //TODO: Import functionality
         importButtonPanel.add(importButton, BorderLayout.WEST);
         headerPanel.add(importButtonPanel, BorderLayout.WEST);
 
         // Horizontal Divider
         JSeparator separator3 = new JSeparator(SwingConstants.HORIZONTAL);
-        separator3.setPreferredSize(new Dimension(20, 1));
+        separator3.setPreferredSize(new Dimension(20, 0));
         separator3.setVisible(true);
         headerPanel.add(separator3);
     }
@@ -79,25 +83,31 @@ public class DaftarMemberPanel extends JPanel{
     private void setupTable(){
         // Table
         scrollListPanel = new JScrollPane();
-        scrollListPanel.setBackground(Color.LIGHT_GRAY);
-        scrollListPanel.setPreferredSize(new Dimension(1045,515));
+        scrollListPanel.setBackground(Color.WHITE);
+        scrollListPanel.setPreferredSize(new Dimension(900,450));
+        scrollListPanel.setBorder(BorderFactory.createEmptyBorder());
         // Cell Renderer
 
-
         JTable itemList = new JTable(getData(), getColumnNames());
-        itemList.setRowHeight(60);
+        itemList.setRowHeight(50);
         itemList.setDefaultEditor(Object.class, null); // make cells not editable
-        itemList.getColumnModel().getColumn(0).setPreferredWidth(300);
+        itemList.getColumnModel().getColumn(0).setPreferredWidth(100);
         itemList.getColumnModel().getColumn(1).setPreferredWidth(300);
-        itemList.getColumnModel().getColumn(2).setPreferredWidth(100);
-        itemList.getColumnModel().getColumn(3).setPreferredWidth(100);
-        itemList.getColumnModel().getColumn(4).setPreferredWidth(100);
+        itemList.getColumnModel().getColumn(2).setPreferredWidth(200);
+        itemList.getColumnModel().getColumn(3).setPreferredWidth(200);
+        itemList.getColumnModel().getColumn(4).setPreferredWidth(200);
         itemList.getColumnModel().getColumn(5).setPreferredWidth(200);
+        itemList.setBorder(BorderFactory.createEmptyBorder());
+        itemList.setShowVerticalLines(false);
+        itemList.setBackground(Colors.WHITE);
+        itemList.setPreferredScrollableViewportSize(itemList.getPreferredSize());
         scrollListPanel.setViewportView(itemList);
+        scrollListPanel.getVerticalScrollBar().setUI(new PlainScrollBar(Colors.WHITE, Colors.SIDE_SLIDER_BLUE));
 
         // Set transparent table lines
         itemList.setGridColor(new Color(240, 240, 240));
         itemList.setIntercellSpacing(new Dimension(0, 5));
+        itemList.setFillsViewportHeight(true);
 
         TableCellRenderer headerRenderer = new TableCellRenderer() {
             JLabel label;
@@ -107,7 +117,7 @@ public class DaftarMemberPanel extends JPanel{
                     label = new JLabel();
                     label.setHorizontalAlignment(JLabel.CENTER);
                     label.setOpaque(true);
-                    label.setBackground(new Color(0xDAE2FF));
+                    label.setBackground(new Color(0xEF0D4F)); //warna judul kolom
                     label.setForeground(new Color(0x243C94)); // set the color of the header text
                     label.setFont(label.getFont().deriveFont(Font.BOLD)); // set the font style to bold
                 }
@@ -118,7 +128,11 @@ public class DaftarMemberPanel extends JPanel{
         for (int i = 0; i < itemList.getColumnModel().getColumnCount(); i++) {
             itemList.getColumnModel().getColumn(i).setHeaderRenderer(headerRenderer);
         }
-        class ButtonRenderer extends JButton implements TableCellRenderer {
+
+        // ini yg buat ngatur button perlihatkan
+        class ButtonRenderer extends JButton implements TableCellRenderer, ActionListener {
+            private JButton contohButton;
+            private boolean isButtonClicked;
             public ButtonRenderer() {
                 setOpaque(true);
                 setBackground(new Color(0x4C6EDF));
@@ -126,49 +140,70 @@ public class DaftarMemberPanel extends JPanel{
                 setPreferredSize(new Dimension(25, 20));
                 setBorderPainted(false);
                 setBorder(BorderFactory.createEmptyBorder(4, 8, 4, 8));
+
+                contohButton = new JButton("Contoh");
+                contohButton.addActionListener(this);
+                isButtonClicked = false;
             }
-        
+
+            public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
+
+                if (isSelected && column == 5 && hasFocus) {
+                    if (!isButtonClicked) {
+                        contohButton.doClick();
+                        setBackground(new Color(0xEE063D));
+                        isButtonClicked = true;
+                    }
+                } else {
+                    setBackground(new Color(0x4C6EDF));
+                    isButtonClicked = false;
+                }
+
+                if (column == 5) {
+                    setText("Perlihatkan");
+                }
+
+                return this;
+            }
+
+
+            public void actionPerformed(ActionEvent e) {
+                if (e.getSource() == contohButton) {
+                    JOptionPane.showMessageDialog(null, "Contoh button activated!");
+                }
+            }
+        }
+
+
+
+        // ini buat button edit
+        class ButtonRenderer2 extends JButton implements TableCellRenderer {
+            public ButtonRenderer2() {
+                setOpaque(true);
+                setPreferredSize(new Dimension(25, 25));
+                setBorderPainted(false);
+                setBorder(BorderFactory.createEmptyBorder(4, 8, 4, 8));
+                ImageIcon icon = new ImageIcon(ResourcePath.ICON + "/edit.png");
+                Image image = icon.getImage();
+                Image newImage = image.getScaledInstance(25,25, Image.SCALE_SMOOTH);
+                icon = new ImageIcon(newImage);
+                setIcon(icon); // set the icon of the button
+            }
+
             public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
                 if (isSelected) {
-                    setBackground(new Color(0xCDD6F6));
+                    setBackground(Colors.TABLE_SELECTED);
                 }
                 if (!isSelected){
-                    setBackground(new Color(0x4C6EDF));
+                    setBackground(Colors.WHITE);
                 }
-                if (value instanceof String) {
-                    setText((String)value);
-                } else if (value instanceof JButton) {
-                    setText(((JButton)value).getText());
-                    setIcon(((JButton)value).getIcon());
-                }
-        
                 return this;
             }
         }
 
-        class ButtonRenderer2 extends JButton implements TableCellRenderer {
-            public ButtonRenderer2() {
-                setOpaque(true);
-                setPreferredSize(new Dimension(25, 20));
-                setBorderPainted(false);
-                setBorder(BorderFactory.createEmptyBorder(4, 8, 4, 8));
-                setIcon(new ImageIcon("../../../../../resources/edit_button.png")); // set the icon of the button
-            }
-        
-            public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
-                if (isSelected) {
-                    setBackground(new Color(0xCDD6F6));
-                }
-                if (!isSelected){
-                    setBackground(new Color(0x4C6EDF));
-                }
-                return this;
-            }
-        }
-        
         TableColumn column = itemList.getColumnModel().getColumn(5);
         column.setCellRenderer(new ButtonRenderer());
-        
+
         TableColumn column2 = itemList.getColumnModel().getColumn(4);
         column2.setCellRenderer(new ButtonRenderer2());
 
@@ -177,35 +212,62 @@ public class DaftarMemberPanel extends JPanel{
         tableHeader.setDefaultRenderer(headerRenderer);
         tableHeader.setAlignmentX(10);
         tableHeader.setFont(tableHeader.getFont().deriveFont(Font.BOLD, 14));
-        tableHeader.setPreferredSize(new Dimension(994, 43));
+        tableHeader.setPreferredSize(new Dimension(600, 43));
     }
-    JButton editButton = new JButton(new ImageIcon("../../../../../resources/edit_button.png"));
+//    JButton editButton = new JButton(new ImageIcon(ResourcePath.ICON + "/edit.png"));
+//    JButton perlihatkanButton = new JButton("Perlihatkan");
 
+    //TODO: Edit and history button functionality
     private static Object[][] getData() {
+        JButton editButton = new JButton(new ImageIcon(ResourcePath.ICON + "/edit.png"));
+        editButton.setBackground(new Color(0xFFFFFF));
+        editButton.setForeground(Color.WHITE);
+        editButton.setBorderPainted(false);
+        editButton.addMouseListener(new MouseAdapter() {
+            public void mouseEntered(MouseEvent e) {
+                editButton.setBackground(new Color(0xCDD6F6));
+            }
+            public void mouseExited(MouseEvent e) {
+                editButton.setBackground(new Color(0x4C6EDF));
+            }
+        });
+        editButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                // TODO: Handle button click event
+            }
+        });
+
+        JButton perlihatkanButton = new JButton("Perlihatkan");
+        PressedButton buttonUI = new PressedButton(new Color(45,77,182));
+        perlihatkanButton.setBackground(new Color(0x4C6EDF));
+        perlihatkanButton.setForeground(Color.WHITE);
+        perlihatkanButton.setBorderPainted(false);
+        perlihatkanButton.setOpaque(true);
+        perlihatkanButton.setFocusable(false);
+        perlihatkanButton.setPreferredSize(new Dimension(130,38));
+        perlihatkanButton.setUI(buttonUI);
+        perlihatkanButton.addMouseListener(new MouseAdapter() {
+            public void mouseEntered(MouseEvent e) {
+                perlihatkanButton.setBackground(new Color(0xCDD6F6));
+            }
+            public void mouseExited(MouseEvent e) {
+                perlihatkanButton.setBackground(new Color(0x070303));
+            }
+        });
+        perlihatkanButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                // TODO: Handle button click event
+            }
+        });
+
         Object[][] data = {
-                {" ", "jennie", "0812", "vip", new JButton(new ImageIcon("../../../../../resources/edit_button.png")), new JButton("Perlihatkan")},
-                {" ", "rose", "021", "member", new JButton(new ImageIcon("path/to/edit/icon.png")),  new JButton("Perlihatkan")},
-                {" ", "jisoo", "0896", "vip", "", new JButton("Perlihatkan")},
-                {" ", "lisa", "911", "member", "", new JButton("Perlihatkan")},
-                {" ", "jennie", "0812", "vip", "", new JButton("Perlihatkan")},
-                {" ", "rose", "021", "member", "", new JButton("Perlihatkan")},
-                {" ", "jisoo", "0896", "vip", "", new JButton("Perlihatkan")},
-                {" ", "lisa", "911", "member", "", new JButton("Perlihatkan")},
-                {" ", "jennie", "0812", "vip", "", new JButton("Perlihatkan")},
-                {" ", "rose", "021", "member", "", new JButton("Perlihatkan")},
-                {" ", "jisoo", "0896", "vip", "", new JButton("Perlihatkan")},
-                {" ", "lisa", "911", "member", "", new JButton("Perlihatkan")},
-                {" ", "jennie", "0812", "vip", "", new JButton("Perlihatkan")},
-                {" ", "rose", "021", "member", "", new JButton("Perlihatkan")},
-                {" ", "jisoo", "0896", "vip", "", new JButton("Perlihatkan")},
-                {" ", "lisa", "911", "member", "", new JButton("Perlihatkan")},
-                {" ", "jennie", "0812", "vip", "", new JButton("Perlihatkan")},
-                {" ", "rose", "021", "member", "", new JButton("Perlihatkan")},
-                {" ", "jisoo", "0896", "vip", "", new JButton("Perlihatkan")},
-                {" ", "lisa", "911", "member", "", new JButton("Perlihatkan")},
+                {" ", "jennie", "0812", "vip", editButton, perlihatkanButton},
+                {" ", "rose", "021", "member", editButton, perlihatkanButton},
+                {" ", "jisoo", "0896", "vip", editButton, perlihatkanButton},
         };
         return data;
     }
+
 
     private static String[] getColumnNames() {
         String[] columnNames = {"ID", "Nama", "No.Telepon", "Kategori", "Edit", "Riwayat"};
