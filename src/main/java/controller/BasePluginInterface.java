1ab9edcb7cb18878247e625018195c01b4a904c6
Alisha Listya <alisha.listya@gmail.com>
2023-05-07 18:26:45 +0700

    fix: Base Plugin Interface

--- src/main/java/controller/BasePluginInterface.java
+++ src/main/java/controller/BasePluginInterface.java
@@ -9,7 +9,6 @@ import java.util.List;
 public interface BasePluginInterface {
 
     public void setupMainWindow(MainWindow mainWindow);
-    public JPanel loadPanel(MainController mainController);
     public void addURL(List<URL> urls);
     public void loadPlugin(MainWindow mainWindow);
 }
